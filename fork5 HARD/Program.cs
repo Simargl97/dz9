fc294519b045bb07c54abad92bdc00086fc05bbc
Sergie.Osipchuk <sergie.osipchuk@mail.ru>
2023-03-13 19:20:13 +0300

    fareeee

---
+++ fork5 HARD/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите любое число(p.s. Лучше более 9) ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+
+if (num >= 10)
+{
+    while (num > 99)
+    {
+        num = num /10;
+    }
+    int result = num % 10;
+    Console.Write(result);
+}
+else
+{
+    Console.Write("Введенное число меньше даже не двухзначное");
+}
