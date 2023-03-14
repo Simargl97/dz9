6001f5ec839fc773d1c86f406625ab69cca72450
Sergie.Osipchuk <sergie.osipchuk@mail.ru>
2023-03-14 20:57:04 +0300

    les3sem2.1

--- lesson3Seminar/les3sem2.0/Program.cs
+++ lesson3Seminar/les3sem2.0/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+
+
+
+/* string CheckCoord(int x)
+{
+    if (x==1)
+        Console.WriteLine("X>0 и Y>0");
+    else if (x==2)
+        Console.WriteLine("X>0 и Y<0");
+    else if (x==3)
+        Console.WriteLine("X<0 и Y<0");
+    else if (x==4)
+        Console.WriteLine("X<0 и Y>0");
+    else
+        Console.WriteLine("The point is located on the coordinate axis"); */
+/*  Console.WriteLine("Введите номер четверти");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(CheckCoord(x)); 
+
+
+
+
+string CheckCoord(int x)
+{
+    string res = "0";
+    if (x==1)
+        res = "X>0 и Y>0";
+    else if (x==2)
+        res = "X>0 и Y>0";
+    else if (x==3)
+        res = "X>0 и Y>0";
+    else if (x==4)
+        res = "X>0 и Y>0";
+    return res;
+}
+
+
+try
+{
+    // Код, который может вызвать исключение
+}
+catch 
+{
+    // Код для обработки исключения
+} */
