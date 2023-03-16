b8c6f333823be5033897a17774d8a04130a89b24
Sergie.Osipchuk <sergie.osipchuk@mail.ru>
2023-03-16 19:26:35 +0300

    dz 2.0

---
+++ dz 2/Program.cs
@@ -0,0 +1,18 @@
+﻿int Prompt (string message);
+{
+    bool message = false;
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трехзначнное число >");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначнное число, пожалуйста повторите ввод");
+    return;
+}
+Console.WriteLine($"Введенное число '{number}'");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра  '{secondRank}'");
