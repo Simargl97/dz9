cda263579714b531e74c18d06b5bf0bd79233f9f
Sergie.Osipchuk <sergie.osipchuk@mail.ru>
2023-03-24 20:51:28 +0300

    lesson 4.1

---
+++ lesson4/lesson4.1/Program.cs
@@ -0,0 +1,67 @@
+﻿/* string[,] table = new string [2, 5];
+// String.Empty
+table[1, 2] = "clovo";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+} */
+
+
+
+
+/*  void PrintArray(int[,] matr)
+
+
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+} */
+
+/* void FillArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix); 
+
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+FillImage(13, 13) //Точка внутри изображения
+FillArray(pic); */
+
+int Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(5));
