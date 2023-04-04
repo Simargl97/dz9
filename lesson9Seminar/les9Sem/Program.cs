3e394d45c6f0ea5b01ef981fd395877face28fde
Sergie.Osipchuk <sergie.osipchuk@mail.ru>
2023-04-04 20:34:49 +0300

    dz00

---
+++ lesson9Seminar/les9Sem/Program.cs
@@ -0,0 +1,29 @@
+﻿// посчитаем сумму всех целых чисел от 1 до N, где N вводится 
+//пользователем
+
+int Summa(int num)
+{
+    int sum = 0;
+    while (true)
+    {
+        if (num == 0) break;
+        sum = sum + num;
+        num = num - 1;
+    }
+    return sum;
+}
+
+int SummaRec(int num)
+{
+    if (num == 0) return 0;
+    return num + SummaRec(num - 1);
+}
+
+//SummaRec(5)
+// 5 + ( 4 + (3 + (2 + (1 + 0))))
+
+
+Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"искомая сумма равна {Summa(num)}");
+Console.WriteLine($"искомая сумма  рекурсией равна {SummaRec(num)}");
