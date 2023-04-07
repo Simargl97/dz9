7f286191742b1f68ebc48c37d78871235567e5d2
Sergie <sergie.osipchuk@mail.ru>
2023-04-07 13:44:33 +0300

    DZ9

---
+++ dz9/dz9.3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+int m = PrintArray("Введите m: ");
+int n = PrintArray("Введите n: ");
+
+int MethodAccermana = Accerman(m, n);
+
+Console.Write($"Функция Аккермана = {MethodAccermana} ");
+
+int Accerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Accerman(m - 1, 1);
+  else return Accerman(m - 1, Accerman(m, n - 1));
+}
+
+int PrintArray(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
